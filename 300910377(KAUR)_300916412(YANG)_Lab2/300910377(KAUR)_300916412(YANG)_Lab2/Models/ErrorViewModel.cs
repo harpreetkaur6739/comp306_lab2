84f8b7a6a2f2f0073bd791989ee91843c19a5806
Harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2018-10-10 17:40:53 -0400

    comp 306 lab 2 Project Structure

---
+++ 300910377(KAUR)_300916412(YANG)_Lab2/300910377(KAUR)_300916412(YANG)_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _300910377_KAUR__300916412_YANG__Lab2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
